d10ce961d59416818cd4d966b50156e210a43c68
EnigmaK9 <enigmak9@protonmail.com>
2023-02-06 04:30:24 -0600

    deleted scrap py

--- scrap.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = "https://preview.redd.it/8c9ldxu5ygga1.png?width=1024&format=png&auto=webp&v=enabled&s=956f4379fe9f5b506d4c922ed19634771ea524d0"
-response = requests.get(url)
-open("image.png", "wb").write(response.content)
