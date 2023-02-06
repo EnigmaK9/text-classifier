cd1d400ed5e441bdbc6f79d670098145dbc67b40
EnigmaK9 <enigmak9@protonmail.com>
2023-02-06 03:44:23 -0600

    text classifier

--- text-classifier.py
+++ text-classifier.py
@@ -1,27 +1,47 @@
+import tkinter as tk
+from tkinter import filedialog
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# Cargar los datos
-df = pd.read_csv("/dataset/IMDB_Dataset.csv")
+# Función para cargar los datos desde un archivo CSV
+def load_data():
+    file_path = filedialog.askopenfilename()
+    df = pd.read_csv(file_path)
+    return df
 
-# Dividir los datos en entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(df["review"], df["sentiment"], test_size=0.2)
+# Función para entrenar y evaluar el modelo
+def train_model(df):
+    # Dividir los datos en entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(df["review"], df["label"], test_size=0.2)
 
-# Convertir los datos de texto en una matriz de características
-vectorizer = CountVectorizer()
-X_train_features = vectorizer.fit_transform(X_train)
-X_test_features = vectorizer.transform(X_test)
+    # Convertir los datos de texto en una matriz de características
+    vectorizer = CountVectorizer()
+    X_train_features = vectorizer.fit_transform(X_train)
+    X_test_features = vectorizer.transform(X_test)
 
-# Entrenar un modelo de regresión logística
-clf = LogisticRegression()
-clf.fit(X_train_features, y_train)
+    # Entrenar un modelo de regresión logística
+    clf = LogisticRegression()
+    clf.fit(X_train_features, y_train)
 
-# Hacer predicciones en los datos de prueba
-y_pred = clf.predict(X_test_features)
+    # Hacer predicciones en los datos de prueba
+    y_pred = clf.predict(X_test_features)
 
-# Evaluar el rendimiento del modelo
-acc = accuracy_score(y_test, y_pred)
-print("Accuracy:", acc)
+    # Evaluar el rendimiento del modelo
+    acc = accuracy_score(y_test, y_pred)
+    result_label.config(text="Accuracy: " + str(acc))
+
+# Configurar la GUI
+root = tk.Tk()
+root.title("Text Classifier GUI")
+
+load_data_button = tk.Button(root, text="Load Data", command=load_data)
+load_data_button.pack()
+
+train_model_button = tk.Button(root, text="Train Model", command=train_model)
+train_model_button.pack()
+
+result_label = tk.Label(root, text="")
+result_label.pack()
